0adfd4a993e3ae7f837a50779ee314c836f9ef9b
Bill Wilcox <bill.wilcox@puppet.com>
2020-03-03 13:23:59 -0700

    Empty check

--- plans/tools/available_language.pp
+++ plans/tools/available_language.pp
@@ -29,7 +29,7 @@ plan common_code::tools::available_language
   }
 
   # take out any undef values
-  $filtered_results = $results.filter | $k, $v | { $v.each | $m | { if defined($m) { $m }}}
+  $filtered_results = $results.filter | $k, $v | { $v.each | $m | { unless empty($m) { $m }}}
 
   notice("Installed languages: ${filtered_results}")
 
